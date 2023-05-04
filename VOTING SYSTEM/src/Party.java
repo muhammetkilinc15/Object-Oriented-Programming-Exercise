5a16abaa45b94181b0a26d9197e5f8e8427f94af
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2023-05-04 21:25:47 +0300

    update

--- VOTING SYSTEM/src/Party.java
+++ VOTING SYSTEM/src/Party.java
@@ -1,7 +1,7 @@
 public class Party {
     private String name;
     private int partyID;
-    int totalNumOfVotes;
+    private int totalNumOfVotes;
     public Party(String name,int partyID)
     {
         this.name=name;
